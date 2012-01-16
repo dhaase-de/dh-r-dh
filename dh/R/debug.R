bd4d0259806d75527910f8bac60e22161eb26c78
Daniel Haase <git@dhaase.de>
2012-01-16 13:48:41 +0000

    R: * imported version 0.4.4 of package 'dh'

--- dh/R/debug.R
+++ dh/R/debug.R
@@ -1,44 +1,4 @@
 # list objects and their size
-"lso" <- function() {
-   objects <- ls(1)
-   objects.data <- matrix("", nrow = length(objects), ncol = 4)
-   colnames(objects.data) <- c("Object", "Class", "Dimension", "Size")
-   for (i in seq(along.with = objects)) {
-      object.name <- objects[i]
-      objects.data[i,] <- c(
-         object.name,
-         paste(class(get(object.name)), collapse = ","),
-         ifelse(
-            is.vector(get(object.name)),
-            paste("<", length(get(object.name)), ">", sep = ""),
-            ifelse(
-               is.array(get(object.name)),
-               paste("<", paste(dim(get(object.name)), collapse = " x "), ">", sep = ""),
-               ""
-            )
-         ),
-         unclass(object.size(get(object.name)))
-      )
-   }
-   
-   objects.sizes <- as.numeric(objects.data[,4])
-   total.size <- sum(objects.sizes)
-   objects.data <- data.frame(objects.data[order(objects.sizes, decreasing = TRUE),], stringsAsFactors = FALSE)
-   
-   for (i in seq(along.with = objects)) {
-      object.size <- as.numeric(objects.data[i,4])
-      objects.data[i,4] <- paste(
-         round(object.size / 1024 / 1024, 2),
-         " MB (",
-         round(100 * object.size / total.size),
-         "%)",
-         sep = ""
-      )
-   }
-   
-   objects.data
-}
-
 "objects.sizes" <- function(environment = 1, plot = TRUE, threshold = 0.05) {
    objects <- ls(name = environment)
    sizes <- sapply(objects, function(object.name) unclass(object.size(get(object.name))))
@@ -57,10 +17,31 @@
 M <- matrix(1:16, nrow = 4)
 
 # print name and value of variables
-"out" <- function(object, ...) {
-   cat(deparse(substitute(object)), " = ", object, "\n", sep = "")
-   if (length(list(...)) > 0L) {
-      out(...)
+"out" <- function(...) {
+   "args.names" <- function(object, ...) {
+      object.name <- deparse(substitute(object))
+      if (length(list(...)) > 0L) {
+         c(object.name, args.names(...))
+      } else {
+         object.name
+      }
+   }
+   
+   # get arguments and their names
+   args <- list(...)
+   names <- args.names(...)
+   names.lengths <- strlen(names)
+   names.maxLength <- max(names.lengths)
+   
+   # show objects
+   for (i in seq(length(names))) {
+      if (names.lengths[i] < names.maxLength) {
+         name.sep <- " "
+      } else {
+         name.sep <- ""
+      }
+      cat(">>> ", fill(paste(names[i], name.sep, sep = ""), to.length = names.maxLength, with.character = ".", from.left = FALSE), " <<< ", sep = "")
+      str(args[[i]], nest.lev = 1, indent.str = "      ")
    }
 }
 

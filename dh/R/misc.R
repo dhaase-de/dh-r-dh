218b3481bdece3251531eee8014b790dd4a2f975
Daniel Haase <git@dhaase.de>
2012-02-17 09:52:44 +0000

    R/packages/dh: * added function 'match.args' which behaves similar to 'match.arg' with   'several.ok=TRUE' but with some more security checks/features

--- dh/R/misc.R
+++ dh/R/misc.R
@@ -18,6 +18,35 @@
    !isTRUE(...)
 }
 
+# wrapper for 'match.arg' which behaves similar to 'several.ok=TRUE' but with some more security checks
+# (e.g. unmatched args trigger errors, 'args=NULL' triggers an error, result only has unique elements)
+"match.args" <- function(args, choices, args.reorder = TRUE, allow.null = FALSE) {
+   # match each element of 'arg' to 'choices'
+   matches <- unlist(lapply(args, function(arg) {
+      match.arg(arg, choices, several.ok = FALSE)
+   }))
+   
+   # check if argument was NULL
+   if (length(matches) == 0L) {
+      if (isTRUE(allow.null)) {
+         return(NULL)
+      } else {
+         stop("Argument 'args' must not be 'NULL'")
+      }
+   }
+   
+   # remove duplicate matches
+   matches <- unique(matches)
+   
+   # reorder matched elements like in argument 'choices'
+   if (isTRUE(args.reorder)) {
+      matches <- choices[which(choices %in% matches)]
+   }
+   
+   # return matches
+   matches
+}
+
 # alias for 'seq_along' or 'seq(along=)'
 "along" <- function(along.with) {
    seq_along(along.with = along.with)
